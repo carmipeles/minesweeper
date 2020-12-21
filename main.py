a065606c0c6fbb40e708a32b9d3231c9af34d9cf
Carmi Peles <carmi545@gmail.com>
2020-12-21 21:36:45 +0200

    finish game when needed

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from minesweeper import MineSweeper
 
 m = MineSweeper()
-m.set_mines()
+m.set_mines(1)
 game_over = False
 while not game_over:
     m.print()
     x = int(input("enter x:"))
     y = int(input("enter y:"))
-    move = input("enter 'o' for open, 'f' for flag or a 'd' for discover:")
-    if move == 'o':
-        game_over = m.click(x, y)
-    elif move == 'f':
-        m.toggle_flag(x, y)
-    elif move == 'd':
-        m.discover(x, y)
-    else:
-        print("invalid option, please try again.")
+    move_type = input("enter 'o' for open, 'f' for flag or a 'd' for discover:")
+    game_over = m.move(move_type, x, y)
--- minesweeper.py
+++ minesweeper.py
@@ -1,40 +1,56 @@
 import random
 from spot import Spot
 
+
 class MineSweeper(object):
-    def __init__(self, total_mines=10, size=10):
-        if total_mines > size*size:
-            raise ValueError
-        self.total_mines = total_mines
+    def __init__(self, size=10):
         self.size = size
         for i in range(size):
             for j in range(size):
                 Spot(i, j, False)
         self.spots = Spot.spots
 
-    def set_mines(self):
+    def set_mines(self, count=10):
         # TODO: set mines only after the first move
+        if count > self.size*self.size:
+            raise ValueError
         l = list(self.spots.values())
         random.shuffle(l)
-        for s in l[:self.total_mines]:
+        for s in l[:count]:
             s.is_mine = True
 
+    def move(self, move_type, x, y):
+        game_over = False
+        if move_type == 'o':
+            game_over = self.click(x, y)
+        elif move_type == 'f':
+            self.toggle_flag(x, y)
+        elif move_type == 'd':
+            self.discover(x, y)
+        else:
+            print("invalid option, please try again.")
+        if all(s.revealed or (s.is_mine and s.is_flagged) for s in self.spots.values()):
+            print("good game!!")
+            game_over = True
+        return game_over
+
     def click(self, x, y):
-        self.spots[(x,y)].reveal()
-        if self.spots[(x,y)].is_mine:
+        self.spots[(x, y)].reveal()
+        if self.spots[(x, y)].is_mine:
             print("game over!!!")
-            return False
-        return True
+            return True
+        return False
 
     def toggle_flag(self, x, y):
-        self.spots[(x,y)].toggle_flag()
-    
+        self.spots[(x, y)].toggle_flag()
+
     def discover(self, x, y):
-        self.spots[(x,y)].discover()
+        self.spots[(x, y)].discover()
 
     def print(self):
         for i in range(self.size):
-            print([s.print() for s in [self.spots[(i,j)] for j in range(self.size)]])
+            print([s.print() for s in [self.spots[(i, j)]
+                                       for j in range(self.size)]])
         print("")
         print("~~~~~~~~~~~~~~~~~~~~~")
         print("")
